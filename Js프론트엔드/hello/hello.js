981bdf1230d3eb1227a845487727e6daf228401a
SEDO11 <do11anm@naver.com>
2022-01-06 11:09:19 +0900

    220106

--- Js프론트엔드/hello/hello.js
+++ Js프론트엔드/hello/hello.js
@@ -1,3 +1,3 @@
-document.write("Hello World!")
-console.log("Hello World!")
-alert("Hello World!")
+document.write("Hello World!") //웹사이트에 표시
+console.log("Hello World!") //콘솔에 표시
+alert("Hello World!") //경고창으로 표시
